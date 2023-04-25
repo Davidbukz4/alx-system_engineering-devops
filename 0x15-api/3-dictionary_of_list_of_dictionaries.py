651706ddc7443e15ebb60b10d5bed077b85b5fe5
David Egwuatu <davidegwuatu2@gmail.com>
2023-04-25 02:44:26 +0000

    added task 4

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+A module that interact with REST API
+'''
+import json
+import requests
+
+
+api = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    try:
+        users_info = requests.get('{}/users'.format(api)).json()
+        todos_info = requests.get('{}/todos'.format(api)).json()
+        todo_list = []
+        all_data = {}
+        file_name = 'todo_all_employees.json'
+        for user in users_info:
+            user_name = user.get('username')
+            user_id = user.get('id')
+            for info in todos_info:
+                if info.get('userId') == user_id:
+                    todo_list.append(info)
+                data = list(map(
+                        lambda x: {
+                            "task": x.get("title"),
+                            "completed": x.get("completed"),
+                            "username": user_name
+                        }, todo_list))
+                data = {'{}'.format(user_id): data}
+                all_data.update(data)
+        with open(file_name, 'w') as f:
+            json.dump(all_data, f)
+    except Exception as e:
+        print(e)
