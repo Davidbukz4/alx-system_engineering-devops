e6536573cd96700ca567fb3f4dfa3a70f050c14c
David Egwuatu <davidegwuatu2@gmail.com>
2023-05-10 13:22:56 +0000

    modified task 1(i)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,5 @@ import requests
 def number_of_subscribers(subreddit):
     ''' subreddit subscriber '''
     url = 'https://reddit.com/r/' + subreddit + '/about/.json'
-    resp = requests.get(url, headers={'User-Agent': 'dive'},
-                        allow_redirects=False).json()
+    resp = requests.get(url, headers={'User-Agent': 'dive'}).json()
     return resp.get('data', {}).get('subscribers', 0)
